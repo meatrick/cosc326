23e9290e12fc988e8171aaa01786c3b5a77071ba
Patrick Smith <smithpm@usc.edu>
2020-04-17 23:03:00 +1200

    very basic start on etudoe06

--- etude06/etude06.cpp
+++ etude06/etude06.cpp
@@ -1,4 +1,32 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+struct Layer {
+	double scale;
+	unsigned char r, g, b;
+
+	Layer(double scale_, unsigned char r_, unsigned char g_, unsigned char b_) {
+		scale = scale_;
+		r = r_;
+		g = g_;
+		b = b_;
+	}
+};
+
 int main() {
-	
+	vector<Layer*> layers;
+
+	// take input in form of: scale r g b
+	string line;
+
+	while (getline(cin, line)) {
+		double scale;
+		unsigned char r, g, b;
+		stringstream ss(line);
+		line >> scale >> r >> g >> b;
+		Layer* layer = new Layer(scale, r, g, b);
+		layers.push_back(layer);
+	}
 
 }
