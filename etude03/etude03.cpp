d16919dd56c820fa7021c6409d2048521d7cf009
Patrick Smith <smithpm@usc.edu>
2020-04-21 15:37:54 +1200

    cleaned up

--- etude03/etude03.cpp
+++ etude03/etude03.cpp
@@ -109,42 +109,18 @@ int main() {
 
 
 
-
-
-	/*  TESTING FOR SCENARIO OBJECT AND INPUT READING
-	reverse(scenarios.begin(), scenarios.end());
-	while (!scenarios.empty()) {
-		Scenario* s = scenarios.back();
-		scenarios.pop_back();
-
-		vector<int> numbers = s->numbers;
-		cout << "numbers: ";
-		for (int i = 0; i < numbers.size(); i++) {
-			cout << numbers[i] << " ";
-		}
-		cout << endl;
-		cout << "target: " << s->target_value << endl;
-		cout << "mode: " << s->order_mode << endl;
-
-	}
-	*/
-
 	// do computation for each scenario
 	for (Scenario* sc : scenarios) {
 		// iterate through all permutations of list of numbers
 		vector<int> vec_numbers(sc->numbers);
 		int len = vec_numbers.size();
 
-		// int numbers[len];
-		// copy(vec_numbers.begin(), vec_numbers.end(), numbers);
-		// sort(numbers, numbers + len);
 
 		// create a binary string of length equal to the number of operands - 1
 		string bitstring = "";
 		for (int i = 0; i < len - 1; i++) {
 			bitstring += "0";
 		}
-		cout << "first bitstring: " << bitstring << endl;
 		// this binary string starts at zero
 		// var solution = NULL;
 		string solution = "-1";
@@ -156,36 +132,25 @@ int main() {
 			do {
 				// add the first number to the sum
 				int sum = vec_numbers[0];
-				cout << "sum starts with first num: " << sum << endl;
-				cout << "attempting bitstring: " << bitstring << endl;
 				// for each bit of the bitstring, from left to right, do:
 				for (int i = 0; i < len - 1; i++) {
-					cout << "bit " << i << endl;
 					// if the bit reads 0, add the the i+1th number to the sum
 					if (bitstring[i] == '0') {
-						cout << "bit " << i << " is a 0" << endl;
-						cout << "the old sum, " << sum << " + " << vec_numbers[i + 1];
 						sum += vec_numbers[i + 1]; // never out of bounds
-						cout << " makes the new sum: " << sum << endl;
 
 					}
 					// if the bit reads 1, multiply the sum by the i+1th number
 					else if (bitstring[i] == '1') {
-						cout << "bit " << i << " is a 1" << endl;
-						cout << "the old sum, " << sum << " * " << vec_numbers[i + 1];
 						sum *= vec_numbers[i + 1];
-						cout << " makes the new sum: " << sum << endl;
 					}
 				}
 				// if the sum is equal to the goal number, set a flag, save the bitstring, and break from the loop
 				if (sum == sc->target_value) {
-					cout << "Sum equals target!" << endl;
 					solution_found = true;
 					solution = bitstring;
 					break;
 				}
 				else {
-					cout << "iteration ended. The current bitstring is: " << bitstring << endl;
 					// else, continue (increment bitstring)
 					// terminate loop if this iteration was "11....1"
 					size_t found = bitstring.find("0");
@@ -195,7 +160,6 @@ int main() {
 					// otherwise, increment
 					bitstring = increment_bitstring(bitstring);
 
-					cout << "the new bitstring is: " << bitstring << endl;
 				}
 
 			} while (true);
@@ -204,7 +168,6 @@ int main() {
 		// else if normal order of operations, do:
 		else if (sc->order_mode == "N") {
 			// save the original bitstring and make a copy of the bistring
-			int bitstring_len = len - 1;
 
 			// len = size of bitstring + 1 (number of operands)
 
@@ -212,67 +175,40 @@ int main() {
 			// from 0 to the max number that the binary string can go, do:
 			do {
 				vector<int> vec_numbers_cpy(vec_numbers);
-				cout << "attempting bitstring: " << bitstring << endl;
+				int bitstring_len = bitstring.size();
 				string bitstring_cpy = bitstring;
 				int sum = 0;
 
 				// for i = 0 to len - 1, do:
 				for (int i = 0; i < bitstring_len; i++) {
-					cout << "iteration: " << i << endl;
-					cout << "working bitstring copy: " << bitstring_cpy << endl;
 					// if there are only +'s left:
 					size_t find = bitstring_cpy.find("1");
 					if (find == string::npos) {
-						cout << "adding everything together now " << endl;
 						// all the multiplications are gone.  Do a pass and add everything together
 						while (!vec_numbers_cpy.empty()) {
 							sum += vec_numbers_cpy.back();
 							vec_numbers_cpy.pop_back();
 						}
-						cout << "the sum is " << sum << endl;
 						break;
 					}
 					// if the bit = 0:
 					if (bitstring_cpy[i] == '0') {
-						cout << "bit " << i << " of bitstring_cpy " << bitstring_cpy << " is 0" << endl;
 						// continue to the next bit
 						continue;
 					}
 					// else if the bit = 1:
 					else if (bitstring_cpy[i] == '1') {
-						cout << "vector copy size: " << vec_numbers_cpy.size() << endl;
-						cout << "bit " << i << " of bitstring_cpy " << bitstring_cpy << " is 1" << endl;
-						cout << "number at index " << i << " is " << vec_numbers_cpy[i] << endl;
-						cout << "number at index " << i + 1 << " is " << vec_numbers_cpy[i + 1] << endl;
 						vec_numbers_cpy[i] *= vec_numbers_cpy[i + 1];
 						// erase bit i from the bitstring and erase vec[i+1] from the vector of numbers
 						bitstring_cpy.erase(i, 1);
-						// int pos_to_erase = i + 1;
-						// vec_numbers_cpy.erase(vec_numbers_cpy.begin()+pos_to_erase);
-						// erase manually by making a copy of the vector:
-						vector<int> vec;
-						for (int j = 0; j < vec_numbers_cpy.size(); j++) {
-							cout << "TEST" << endl;
-							if (j == i + 1) {
-								cout << "skipping copy at index " << j << endl;
-								continue;
-							}
-							cout << "pushing " << vec_numbers_cpy[j] << endl;
-							vec.push_back(vec_numbers_cpy[j]);
-						}
-						vec_numbers_cpy = vec;
+						int pos_to_erase = i + 1;
+						vec_numbers_cpy.erase(vec_numbers_cpy.begin() + pos_to_erase);
 
-						// cout << "Find seg fault" << endl;
 						bool repeat = true;
-						bitstring_len--;
-						cout << "the bitstring is now: " << bitstring_cpy << endl;
-						cout << "bitstring len is now: " << bitstring_len << endl;
-						cout << "after multiplcation, number " << i << " is " << vec_numbers_cpy[i] << endl;
-						//<< " and number " << i+1 << " is " << vec_numbers_cpy[i+1] << endl; casues out of bounds error
+						bitstring_len = bitstring_cpy.size();
 
 						// if (repeat)
 						if (repeat) {
-							cout << "repeating" << endl;
 							i = i - 2;
 							repeat = false;
 							continue;
@@ -281,7 +217,6 @@ int main() {
 				}
 				// if sum == goal:
 				if (sum == sc->target_value) {
-					cout << "we've got a match!" << endl;
 					// save original bitstring, set a flag, break from the loop
 					solution = bitstring;
 					solution_found = true;
@@ -295,9 +230,7 @@ int main() {
 						break;
 					}
 					// otherwise, increment
-					cout << "the bitstring is incremented from " << bitstring;
 					bitstring = increment_bitstring(bitstring);
-					cout << " to " << bitstring << endl;
 				}
 
 			} while (true);
