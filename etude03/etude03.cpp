b340545e84f05f228cbf60fda74176a746f6af24
Jenna Smith <jennasmith@Jennas-MacBook-Pro-2.local>
2020-05-21 10:50:20 -0700

    intermediate commit because VS is freezing

--- etude03/etude03.cpp
+++ etude03/etude03.cpp
@@ -80,37 +80,37 @@ public:
 };
 
 
-Node* DFS(vector<Node*> tree, Node* start_node, int target_value) {
-	start_node->visited = true;
-
+vector<char> DFS(vector<int> input_numbers, size_t level, int partial_sum, vector<char> operators, int target_value) {
 	// base case: if start_node is a leaf
-	if (start_node->index >= tree.size() / 2) {
-		if (start_node->product == target_value) {
-			return start_node;
+	if (level == input_numbers.size() - 1) {
+		if (partial_sum == target_value) {
+			return operators;
 		}
 	}
 	else {
-		int l_child_index = start_node->index * 2 + 1;
-		int r_child_index = start_node->index * 2 + 2;
-		Node* l_child = tree[l_child_index];
-		Node* r_child = tree[r_child_index];
+		level++;
+		vector<char> solution;
 
-		if (!l_child->visited) {
-			l_child->product += start_node->product;
-			Node* solution = DFS(tree, l_child, target_value);
-			if (solution) {
-				return solution;
-			}
+		// left child
+		int left_partial_sum = partial_sum + input_numbers[level]; 
+		operators.push_back('+');
+		vector<char> solution = DFS(input_numbers, level, left_partial_sum, operators, target_value);
+		if (!solution.empty()) {
+			return solution;
 		}
-		if (!r_child->visited) {
-			r_child->product *= start_node->product;
-			Node* solution = DFS(tree, r_child, target_value);
-			if (solution) {
-				return solution;
-			}
+	
+		int right_partial_sum = partial_sum * input_numbers[level];
+		operators.push_back('*');
+		vector<char> solution = DFS(input_numbers, level, right_partial_sum, operators, target_value);
+		if (!solution.empty()) {
+			return solution;
 		}
+
 	}
-	return NULL;
+	// signal that the DFS did not find a valid solution here
+	vector<char> empty_vec;
+	empty_vec.clear();
+	return empty_vec;
 }
 
 NodeN* DFSN(vector<NodeN*> tree, NodeN* start_node, int target_value) {
@@ -156,58 +156,16 @@ NodeN* DFSN(vector<NodeN*> tree, NodeN* start_node, int target_value) {
 }
 
 // find solution for type L
-vector<char> find_solution(vector<Node*> tree, int target_value) {
-	Node* root = tree[0];
-
-	Node* node = DFS(tree, root, target_value);
+vector<char> find_solution(vector<int> input_numbers, int target_value) {
 
 	vector<char> operators;
+	vector<char> solution = DFS(input_numbers, 0, input_numbers[0], operators, target_value);
 
-	if (!node) {
+	// no solution
+	if (operators.empty()) {
 		operators.push_back('0');
-		return operators;
 	}
 
-
-
-	vector<Node*> path;
-	path.push_back(node);
-
-	// get parent
-	while (true) {
-		Node* parent;
-		if (node->index == 0) {
-			parent = NULL;
-		}
-		else if (node->index % 2 == 1) {
-			parent = tree[node->index / 2];
-		}
-		else if (node->index % 2 == 0) {
-			parent = tree[node->index / 2 - 1];
-		}
-		if (parent) {
-			node = parent;
-			path.push_back(node);
-		}
-		else {
-			break;
-		}
-	}
-
-	reverse(path.begin(), path.end());
-
-	for (int i = 0; i < path.size(); i++) {
-		Node* parent = path[i];
-		if (parent->index < tree.size() / 2) {
-			Node* child = path[i + 1];
-			if (child->index == parent->index * 2 + 1) {
-				operators.push_back('+');
-			}
-			else if (child->index == parent->index * 2 + 2) {
-				operators.push_back('*');
-			}
-		}
-	}
 	return operators;
 }
 
@@ -306,17 +264,7 @@ int main() {
 			// all processing and output
 
 			if (s->order_mode == "L") {
-				// create array of nodes representing the tree
-				vector<Node*> tree;
-				int index = 0;
-				for (size_t i = 0; i < input_numbers.size(); i++) {
-					for (size_t j = 0; j < pow(2, i); j++) {
-						tree.push_back(new Node(index, input_numbers[i]));
-						index++;
-					}
-				}
-
-				vector<char> solution_operators = find_solution(tree, s->target_value);
+				vector<char> solution_operators = find_solution(input_numbers, s->target_value);
 				s->set_solution(solution_operators);
 			}
 			else if (s->order_mode == "N") {
