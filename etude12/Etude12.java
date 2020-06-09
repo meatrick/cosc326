9dbf5af1e5fbf553dd40df73e257947d6ad30b48
Jenna Smith <jennasmith@Jennas-MacBook-Pro-2.local>
2020-06-08 23:22:38 -0700

    Etude 12 done

--- etude12/Etude12.java
+++ etude12/Etude12.java
@@ -8,157 +8,195 @@ public class Etude12 {
   ArrayList<Route> routes_to;
   double bestDist;
   ArrayList<City>bestRoute;
-
+  
   public City(String city_name) {
    this.city_name = city_name;
    this.routes_from = new ArrayList<Route>();
    this.routes_to = new ArrayList<Route>();
    this.bestRoute = new ArrayList<City>();
-
-
+   
+   
   }
-
+  
   public int compareTo(City c){
     return Double.compare(bestDist, c.bestDist);
   }
-
   public String getCityName() {
    return city_name;
   }
-
   public void setCityName(String city_name) {
    this.city_name = city_name;
   }
-
+  
   public ArrayList<Route> getRoutesFrom() {
    return routes_from;
   }
-
+  
+  
   public void addRouteFrom(Route route) {
-
-    routes_from.add(route);
-
+    
+    routes_from.add(route);    
+   
   }
-
+  
   public ArrayList<Route> getRoutesTo() {
    return routes_to;
   }
-
+  
   public void addRouteTo(Route route) {
-
-    routes_to.add(route);
-
-
+   
+    routes_to.add(route);    
+    
+   
   }
-
+  
+ 
+  
   public void setBestDist(double bestDist){
    this.bestDist = bestDist;
   }
-
+  
   public double getBestDist() {
    return bestDist;
   }
-
+  
   public void setBestRoute(ArrayList <City> bestRoute){
    this.bestRoute = bestRoute;
-
+   
   }
-
+  
   public void clearBest(){
     this.bestRoute.clear();
   }
-
+  
   public ArrayList<City> getBestRoute(){
    return bestRoute;
   }
-
+  
  }
-
  public static class Route {
   City city_from;
   City city_to;
   double fare;
-
+  
   public Route(City city_from, City city_to, double fare) {
    this.city_from = city_from;
    this.city_to = city_to;
    this.fare = fare;
   }
-
+  
   public double getFare() {
    return fare;
   }
-
+  
   public City getDest(){
     return city_to;
   }
-
+  
+  public City getFrom() {
+	  return city_from;
+  }
+  public City getTo() {
+	  return city_to;
+  }
+  
+  
   public void setDest(City dest){
     this.city_to = dest;
  }
  }
-
+ 
+  
+  
+  
+ 
  public static class RouteMap {
   ArrayList<City> all_cities;
   ArrayList<Route> all_routes;
   ArrayList<String> all_cities_str;
-
+  
   public RouteMap() {
     this.all_cities = new ArrayList<City>();
     this.all_routes  = new ArrayList<Route>();
     this.all_cities_str = new ArrayList<String>();
   }
-
+  
   public int length() {
-
+    
     return all_cities.size();
   }
-
+ 
+  
   // can replace check to improve runtime
-  public void addCity(City city) {
+  public void addCity(City city) { 
    if (!all_cities_str.contains(city.getCityName())) {
     all_cities.add(city);
     all_cities_str.add(city.getCityName());
    }
   }
-
+  
 public void addRoute(City city_from, City city_to, double fare) throws Exception {
    Route routeFrom = new Route(city_from, city_to, fare);
    Route routeTo = new Route(city_to, city_from, fare);
    int q = all_cities_str.indexOf(city_from.getCityName());
-   if(q >= 0){
-     City w = all_cities.get(q);
+   int contain = 1;
+   
+   City w = all_cities.get(q);
+   for(Route c : all_routes) {
+	   if((routeFrom.getTo().getCityName().equals(c.getTo().getCityName())) &&
+			   (routeFrom.getFrom().getCityName().equals(c.getDest().getCityName()))
+			   ||(routeTo.getTo().getCityName().equals(c.getFrom().getCityName())) && 
+			   (routeTo.getFrom().getCityName().equals(c.getTo().getCityName())))
+			   {
+		   System.out.println(city_from.getCityName());
+		   contain = 0;
+		   
+	   }
+   }
+   if(q >= 0 && contain == 1) {// && contain == 1){
+     
      w.addRouteFrom(routeFrom);
-
+     w.addRouteFrom(routeTo); //zzz
+     
    }
-   else{
+   else if(contain == 1){
      city_from.addRouteFrom(routeFrom);
+     city_from.addRouteFrom(routeTo);
    }
-
+   
+   else if (contain == 0) {
+	   System.out.println("Invalid: Non-unique routes");
+   }
+  
    city_to.addRouteTo(routeTo);
-   if (!all_routes.contains(routeTo)) {
+   if (!(all_routes.contains(routeTo) || all_routes.contains(routeFrom))) {
     all_routes.add(routeTo);
    }
-   else {
-    throw new Exception("Invalid: Non-unique routes");
-   }
-
-   if(!all_routes.contains(routeFrom)){
+  // else {
+    //throw new Exception("Invalid: Non-unique routes");
+	 //  System.out.println("not unique1");
+   //}
+   
+   if(!(all_routes.contains(routeFrom) || all_routes.contains(routeTo))){
      all_routes.add(routeFrom);
    }
-   else {
-    throw new Exception("Invalid: Non-unique routes");
-   }
-
+   //else {
+   // throw new Exception("Invalid: Non-unique routes");
+	  // System.out.println("not unique2");
+   //}
+   
   }
-
+  
   public void addRoute(String city_from_str, String city_to_str, double fare) throws Exception {
    City city_from, city_to;
-
+   /*
+    * need to make sure cities dont have the same from and to even if fare is difff
+    * compare before adding
+    */
    if (!all_cities_str.contains(city_from_str)) {
     city_from = new City(city_from_str);
     addCity(city_from);
-
+    
    }
 
     else {
@@ -169,42 +207,45 @@ public void addRoute(City city_from, City city_to, double fare) throws Exception
     city_to = new City(city_to_str);
     addCity(city_to);
 
-
+    
    } else {
     int city_to_index = all_cities_str.indexOf((city_to_str));
     city_to = all_cities.get(city_to_index); //watch indexes
    }
-
-
+   
+   
    addRoute(city_from, city_to, fare);
-
+   
+      
   }
-
-  public boolean containsCityCalled(String city) {
+  
+  
+  public boolean containsCityCalled(String city) { 
     if (all_cities.contains((city))) { //used to be new
     return true;
    }
-
+   
    return false;
   }
-
+  
+  
   // TODO
   public Vector<City> findCheapestRoute(RouteMap rm, City city_from, City city_to) {
    Vector<City> final_route = new Vector<City>();
-   PriorityQueue<City> cities = new PriorityQueue<City>();
+   PriorityQueue<City> cities = new PriorityQueue<City>(); 
    //all_cities.size()
-
+   
    for(int i = 0; i < all_cities.size(); i++) {
      City c = all_cities.get(i);
      c.setBestDist(10000);
      c.setBestRoute(null);
-
+     
    }
-
-
+   
+   
    for(int a = 0; a < all_cities.size(); a++){
      if(all_cities.get(a).getCityName().equals(city_from.getCityName())){
-
+        
         cities.add(all_cities.get(a));
         cities.peek().setBestDist(0);
         final_route.add(all_cities.get(a));
@@ -212,18 +253,18 @@ public void addRoute(City city_from, City city_to, double fare) throws Exception
    }
 
    do {
-
+   
     City v = cities.poll();
     ArrayList <Route> routes= new ArrayList<Route>();
     routes = v.getRoutesFrom();
-
-
-   	for(int i = 0; i < routes.size(); i++) {
-      	Route r = routes.get(i);
-
-
+    
+   
+   for(int i = 0; i < routes.size(); i++) {
+      Route r = routes.get(i);
+ 
+     
      if(r.getDest().getBestDist() > v.getBestDist() + r.getFare()) {
-
+     
       r.getDest().setBestDist(v.getBestDist () + r.getFare());
       cities.add(r.getDest());
       ArrayList<City> br = new ArrayList<City>();
@@ -232,177 +273,192 @@ public void addRoute(City city_from, City city_to, double fare) throws Exception
         br.add(u, v.getBestRoute().get(u));
       }
       }
-
+      
         br.add(r.getDest());
-
+      
       r.getDest().setBestRoute(br);
-
-
+      
+      
 
      }
     }
    } while(!cities.isEmpty());
-
+    
   for(int a = 0; a < all_cities.size(); a++){
      if(all_cities.get(a).getCityName().equals(city_to.getCityName())){
-
-
-
-
-  for(int x = 0; x < all_cities.get(a).getBestRoute().size(); x++){
-      final_route.add(all_cities.get(a).getBestRoute().get(x));
+         
+        
+     
+   
+  for(int x = 0; x < all_cities.get(a).getBestRoute().size(); x++){   
+    final_route.add(all_cities.get(a).getBestRoute().get(x));
    }
      }
   }
 
-
+   
    return final_route;
   }
-
-
+  
+  
  }
+ 
+ 
+ 
 
  // TODO
  public static void main(String [] Args) {
-  // create RouteMap
+  // create RouteMap 
    RouteMap rm = new RouteMap();
-
 
   // debug
 //  File file = new File(".");
 //  for(String fileNames : file.list()) System.out.println(fileNames);
-
+  
   // get filename from stdin
-  // removed reading filename from std in because I think that the file is automatically read
-  // i.e. they are gonna run the program by doing java etude10 < input.txt, so our program automatically
-  // has the input from the file in System.in
-
+  
   City final_city_from = null, final_city_to = null;
-
-
-
+  
+  
+  
   // open file and get input line by line
   // get input, creating Cities and routes and adding to the RouteMap
   try {
    // load file into scanner
-   // TEMPORARY
+   // TEMPORARY 
  //  filename = //"src/Cosc326Pair/" //+ filename;
-
-
-  //C:\Users\Maclean\Desktop\jkjk\fucking.txt
-
+   
+   
+    // program takes input from command line like: java Etude08 < inputFile.txt
    Scanner ifile = new Scanner(System.in);
 
-
+   
+   
    // first line handled separately
-
+   
    // get and handle fist line of input
    String final_city_from_str, final_city_to_str; // load these variables
    String line = ifile.nextLine();
    line = line.toLowerCase();
    String[] inputs = line.split(",");
-
+   
+  /** try{
    if (inputs.length != 2) {
-    throw new Exception("Invalid: route set");
    }
-
-
+   }catch (Exception e){
+    System.out.println("Invalid: route");
+   }
+   
+   **/
+   if(inputs.length != 2){
+     System.out.println("Invalid: route");
+     return;
+   }
+   
+   
+   
    // instantiate Cities
    final_city_from = new City(inputs[0].trim());
    final_city_to = new City(inputs[1].trim());
-
+   
    // add cities to routemap: not necessary?
 //   rm.addCity(final_city_from);
 //   rm.addCity(final_city_to);
-
+   
    // handle the remaining lines of the file
    while (ifile.hasNextLine()) {
-
+    
     line = ifile.nextLine().toLowerCase();
     inputs = line.split(","); // split line into multiple strings, separated by comma
-
-    if (inputs.length != 3) {
-     throw new Exception("Invalid: route");
+    
+    /**try{
+      System.out.println("FUCK");
+    if (inputs.length == 3) {
+      System.out.println("YOU");
     }
-
-
+    }catch(Exception e){
+     System.out.println("Invalid: route set");
+    }
+    **/
+    
+    if(inputs.length != 3){
+      System.out.println("Invalid: route set");
+      return;
+    }
+    
+    
+    
     String city_from_str = inputs[0], city_to_str = inputs[1], fare_str = inputs[2];
     double fare = Double.parseDouble(fare_str.trim());
     rm.addRoute(city_from_str.trim(), city_to_str.trim(), fare);
    }
-   ifile.close();
   } catch(Exception e) {
    System.out.println(e.getMessage());
    e.printStackTrace();
-   ;//TODO: remove
   }
-
+  
+  
   // use the method to find the cheapest route
   Vector<City> final_route = rm.findCheapestRoute(rm, final_city_from, final_city_to);
+  
 
+  
+  // write info to params.txt
+  try {
+    FileWriter fw = new FileWriter("params.txt");
+    // write best route
+    for (int i = 0; i < final_route.size(); i++) {
+      if(final_route.get(i) != null){
+        fw.write(final_route.get(i).city_name);
+      }
+      if (i != final_route.size()-1) {
+        fw.write("-");
+      }
+    }
 
+    fw.write("\n");
 
-  // output the cheapest route
-
-  	// write info to params.txt
-	try {
-		FileWriter fw = new FileWriter("params.txt");
-		// write best route
-		for (int i = 0; i < final_route.size(); i++) {
-			if(final_route.get(i) != null){
-				fw.write(final_route.get(i).city_name);
-			}
-			if (i != final_route.size()-1) {
-				fw.write("-");
-			}
-		}
-
-		fw.write("\n");
-
-		// write all cities
-		for (City city : rm.all_cities) {
-			fw.write(city.city_name);
-			fw.write("\n");
-		}
-		
-		fw.write("Routes:\n");
-		
+    // write all cities
+    for (City city : rm.all_cities) {
+      fw.write(city.city_name);
+      fw.write("\n");
+    }
+    
+    fw.write("Routes:\n");
+    
     // write every other route 
     ArrayList<Route> alreadySeen = new ArrayList<Route>();
-		for (Route route : rm.all_routes) {
+    for (Route route : rm.all_routes) {
       if (alreadySeen.contains(route)) {
         continue;
       }
       alreadySeen.add(route);
-			// route_from, route_to, fare
-			fw.write(route.city_from.city_name);
-			fw.write(" ");
-			fw.write(route.city_to.city_name);
-			fw.write(" ");
+      // route_from, route_to, fare
+      fw.write(route.city_from.city_name);
+      fw.write(" ");
+      fw.write(route.city_to.city_name);
+      fw.write(" ");
       fw.write(Double.toString(route.fare));
       fw.write("\n");
-		}
-		fw.close();
-	} catch (IOException e) {
-		System.err.println("a file error occured");
-		e.printStackTrace();
-	}
-	
-	// execute python script
-	try {
-		String command = "python etude12.py";
-		Process p = Runtime.getRuntime().exec(command);
-	} catch (Exception e) {
-		e.printStackTrace();
-		System.err.println("Error executing python file");
-	}
-
-   
+    }
+    fw.close();
+  } catch (IOException e) {
+    System.err.println("a file error occured");
+    e.printStackTrace();
+  }
   
-
-
-
+  // execute python script
+  try {
+    String command = "python etude12_helper.py";
+    Process p = Runtime.getRuntime().exec(command);
+  } catch (Exception e) {
+    e.printStackTrace();
+    System.err.println("Error executing python file");
+  }
+  
+  
   return;
  }
  }
-
+ 
+ 
---
+++ etude12/etude12_helper.py
@@ -0,0 +1,85 @@
+import sys
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+# Using readlines() 
+file1 = open('params.txt', 'r') 
+Lines = file1.readlines() 
+
+
+# load final route into list of strings
+final_route = Lines[0] 
+cities_on_route = final_route.strip().split("-") # list of cities included in the final route
+
+# create graph
+G = nx.Graph()
+    
+# load all cities into graph
+normal_node_indexes = []
+final_node_indexes = []
+separator_index = -1 # separator splits nodes from routes
+for index in range(1, len(Lines)):
+    line = Lines[index].strip()
+    if line == "Routes:":
+        separator_index = index
+        break
+    else:
+        if line in cities_on_route:
+            final_node_indexes.append(line)
+        else:
+            normal_node_indexes.append(line)
+        G.add_node(line)
+
+# load all routes into graph
+normal_edge_indexes = []
+final_edge_indexes = []
+for index in range(separator_index + 1, len(Lines)):
+    params = Lines[index].strip().split()
+    G.add_edge(params[0], params[1], weight=params[2])
+    str1 = params[0]
+    str0 = params[1]
+    if str0 in cities_on_route and str1 in cities_on_route:
+        index0 = cities_on_route.index(str0)
+        index1 = cities_on_route.index(str1)
+        if index1 == index0 + 1:
+            # special edge!
+            final_edge_indexes.append((params[0], params[1], params[2]))
+        else:
+            # normal edge
+            normal_edge_indexes.append((params[0], params[1], params[2]))
+    else:
+        normal_edge_indexes.append((params[0], params[1], params[2]))
+
+
+# get positiions for nodes/edges
+pos=nx.spring_layout(G)
+
+# draw
+# normal nodes
+nx.draw_networkx_nodes(G, pos, nodelist=normal_node_indexes, node_color='b', node_size=500, alpha=0.5)
+
+# special nodes
+nx.draw_networkx_nodes(G, pos, nodelist=final_node_indexes, node_color='orange', node_size=500, alpha=0.8)
+
+# normal edges
+nx.draw_networkx_edges(G, pos, edgelist=normal_edge_indexes, edge_color='b', width=2, style='dashed', alpha=0.5)
+
+# special edges
+nx.draw_networkx_edges(G, pos, edgelist=final_edge_indexes, edge_color='g', width=2)
+
+# draw edge labels
+weights = nx.get_edge_attributes(G, 'weight')
+nx.draw_networkx_edge_labels(G, pos, weights)
+
+nx.draw_networkx_labels(G, pos, font_size=12)
+
+
+#output to file
+plt.axis('off')
+plt.tight_layout()
+plt.title("Cheapest Bus Route - COSC 326")
+plt.savefig('etude12.png')
+
+
+
